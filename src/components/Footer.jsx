41929c570538259471864b0c7c80ee753569bab5
Amirk9 <mdamrkj99@gmail.com>
2024-07-16 19:22:27 +0530

    Done !!

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer_container">
+        <div className="footer_column">
+          <h3>ONLINE SHOPPING</h3>
+
+          <a href="#">Men</a>
+          <a href="#">Women</a>
+          <a href="#">Kids</a>
+          <a href="#">Home & Living</a>
+          
+        </div>
+
+        
+        <div className="footer_column">
+          <h3>ONLINE SHOPPING</h3>
+
+          <a href="#">Men</a>
+          <a href="#">Women</a>
+          <a href="#">Kids</a>
+          <a href="#">Home & Living</a>
+    
+         
+        </div>
+      </div>
+      <hr />
+
+      <div className="copyright">
+        © ViewMart  All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
